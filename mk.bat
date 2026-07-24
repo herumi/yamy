a286eb1e7e8f0d3dd04bcb446c89b71f63c818b3
MITSUNARI Shigeo <herumi@nifty.com>
2026-07-24 10:01:41 +0900

    generate functions.h unless it exists

--- mk.bat
+++ mk.bat
@@ -1,2 +1,3 @@
+if not exist functions.h cscript //nologo tools\makefunc.js engine.h functions.h
 MSBuild proj\yamy.sln /t:Build /p:Configuration=Release /p:Platform=x64
 MSBuild proj\yamy.sln /t:Build /p:Configuration=Release /p:Platform=Win32
