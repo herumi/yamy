e315ad6cfdeb1e0737154dd8456f1769c60586d8
MITSUNARI Shigeo <herumi@nifty.com>
2026-07-27 10:41:50 +0900

    rename readme.txt to readme.md

--- mk-dist.bat
+++ mk-dist.bat
@@ -17,7 +17,7 @@ call mk.bat
 
 if exist "%ZIP%" del "%ZIP%"
 
-powershell -NoProfile -Command "Compress-Archive -DestinationPath '%ZIP%' -Path .\yamy.ini, .\104.mayu, .\109.mayu, .\default.mayu, .\emacsedit.mayu, .\104on109.mayu, .\109on104.mayu, .\dot.mayu, .\workaround.mayu, .\workaround.reg, .\readme.txt, Release\yamy.exe, Release\yamy32, Release\yamy64, Release\yamy32.dll, Release\yamy64.dll, Release\yamyd32"
+powershell -NoProfile -Command "Compress-Archive -DestinationPath '%ZIP%' -Path .\yamy.ini, .\104.mayu, .\109.mayu, .\default.mayu, .\emacsedit.mayu, .\104on109.mayu, .\109on104.mayu, .\dot.mayu, .\workaround.mayu, .\workaround.reg, .\readme.md, Release\yamy.exe, Release\yamy32, Release\yamy64, Release\yamy32.dll, Release\yamy64.dll, Release\yamyd32"
 
 if errorlevel 1 (
 	echo failed to create %ZIP%
